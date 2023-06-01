6c38a1ec27923b9d9205c8eee1aa1a9da21fdaf7
0jenn0 <jenn0.6n@gmail.com>
2023-06-01 23:00:09 +0900

    nextagram_ver2 Avatar수정

--- nextagram_ver2/src/components/Avatar.tsx
+++ nextagram_ver2/src/components/Avatar.tsx
@@ -1,12 +1,26 @@
-export default function Avatar({ image }: { image?: string | null }) {
+type Props = {
+  image?: string | null;
+  border: boolean;
+};
+
+export default function Avatar({ image, border }: Props) {
+  if (border)
+    return (
+      <div className="w-9 h-9 rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 flex items-center">
+        <img
+          className="rounded-full p-[0.1rem]"
+          alt="user profile"
+          src={image ?? undefined}
+          referrerPolicy="no-referrer"
+        />
+      </div>
+    );
   return (
-    <div className="w-9 h-9 rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 flex items-center">
-      <img
-        className="rounded-full p-[0.1rem]"
-        alt="user profile"
-        src={image ?? undefined}
-        referrerPolicy="no-referrer"
-      />
-    </div>
+    <img
+      className="rounded-full p-[0.1rem]"
+      alt="user profile"
+      src={image ?? undefined}
+      referrerPolicy="no-referrer"
+    />
   );
 }
