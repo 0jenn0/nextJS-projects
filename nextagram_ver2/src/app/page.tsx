da85b96dd3ffa11f6b324c0357e818347a44c09d
0jenn0 <jenn0.6n@gmail.com>
2023-06-03 00:55:31 +0900

    nextagram_ver2 sidebar 구현

--- nextagram_ver2/src/app/page.tsx
+++ nextagram_ver2/src/app/page.tsx
@@ -1,5 +1,14 @@
+import Profile from "@/components/Profile";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Nextagram</h1>;
+  return (
+    <section className="w-full flex justify-between">
+      <section>
+        <h1>FollowingBar</h1>
+        <h1>PostList</h1>
+      </section>
+      <Profile />
+    </section>
+  );
 }
