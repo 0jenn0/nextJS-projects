305667d1881c39dccda22df6c83064e0e76654c0
0jenn0 <jenn0.6n@gmail.com>
2023-06-08 15:37:49 +0900

    nextagram_ver2 Avatar 리팩토링

--- nextagram_ver2/src/components/Avatar.tsx
+++ nextagram_ver2/src/components/Avatar.tsx
@@ -14,9 +14,9 @@ export default function Avatar({
   return (
     <div className={getContainerStyle(size, highlight)}>
       <img
-        className={`bg-white rounded-full p-[0.1rem] object-cover + ${getImageSizeStyle(
-          size
-        )}`}
+        className={`bg-white rounded-full p-[0.1rem] object-cover + ${
+          getImageStyle(size).image
+        }`}
         alt="user profile"
         src={image ?? undefined}
         referrerPolicy="no-referrer"
@@ -25,19 +25,9 @@ export default function Avatar({
   );
 }
 
-const getContainerSize = (size: AvatarSize) => {
-  switch (size) {
-    case "small":
-      return "w-9 h-9";
-    case "medium":
-      return "w-11 h-11";
-    case "large":
-      return "w-[68px] h-[68px]";
-    case "xlarge":
-      return "w-[142px] h-[142px]";
-    default:
-      throw new Error(`Unsupported type size: ${size}`);
-  }
+type ImageSizeStyle = {
+  container: string;
+  image: string;
 };
 
 const getContainerStyle = (size: AvatarSize, highlight: boolean): string => {
@@ -45,21 +35,24 @@ const getContainerStyle = (size: AvatarSize, highlight: boolean): string => {
   const highlightStyle = highlight
     ? "bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300"
     : "";
-  const sizeStyle = getContainerSize(size);
+  const sizeStyle = getImageStyle(size).container;
 
   return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
 };
 
-const getImageSizeStyle = (size: AvatarSize): string => {
+const getImageStyle = (size: AvatarSize): ImageSizeStyle => {
   switch (size) {
     case "small":
-      return "w-[34px] h-[34px] p-[0.1rem]";
+      return { container: "w-9 h-9", image: "w-[34px] h-[34px] p-[0.1rem]" };
     case "medium":
-      return "w-[42px] h-[42px] p-[0.1rem]";
+      return { container: "w-11 h-11", image: "w-[42px] h-[42px] p-[0.1rem]" };
     case "large":
-      return "w-16 h-16 p-[0.2rem]";
+      return { container: "w-[68px] h-[68px]", image: "w-16 h-16 p-[0.2rem]" };
     case "xlarge":
-      return "w-[138px] h-[138px] p-[0.3rem]";
+      return {
+        container: "w-[142px] h-[142px]",
+        image: "w-[138px] h-[138px] p-[0.3rem]",
+      };
     default:
       throw new Error(`Unsupported type size: ${size}`);
   }
